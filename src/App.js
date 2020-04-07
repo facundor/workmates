79bed4818da068e0958995439fff6cda7ebadd5b
Facundo Rodriguez <facundo.rodriguez@fluxit.com.ar>
2020-04-07 10:55:35 -0300

    base commit inicial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+
+import WorkmateList from "./components/WorkmateList";
+import WorkmateService from "./services/WorkmateService";
 
 function App() {
+  const [moneyType, saveMoneyType] = useState("");
+  
+
+  useEffect(() => {
+    if (coinType === "" || moneyType === "") {
+      return;
+    }
+
+    const url = `https://hello-ms.k8s.fluxit.com.ar/users`;
+    const workmateService = new WorkmateService();
+    const workmateList = workmateService.getUsers(url);
+
+    const workmatesListResult = async () => {
+      const value = await workmateList;
+      loadList(value);
+    };
+
+    coinsDetail();
+  }, [moneyType, coinType]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="one-half column">
+          <img src={imagen} alt="imagen criptomonedas" className="logtipo" />
+        </div>
+        <div className="one-half column">
+          <h1>Cotiza Criptomonedas al Instante</h1>
+          <Formulario
+            saveMoneyType={saveMoneyType}
+            saveCoinType={saveCoinType}
+          />
+
+          <Quotation coinInfo={coinInfo} />
+        </div>
+      </div>
     </div>
   );
 }
