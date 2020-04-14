063b04cc0d32eef4fe0068a0378de8bbbf9820b3
Facundo Rodriguez <facundo.rodriguez@fluxit.com.ar>
2020-04-14 16:00:24 -0300

    ajustes para incluir recaptcha

--- src/components/WorkmateList.js
+++ src/components/WorkmateList.js
@@ -1,22 +1,30 @@
 import React, { useState, useEffect } from "react";
 import WorkmateService from "../services/WorkmateService";
 import Workmate from "./Workmate";
+import ReCAPTCHA from "react-google-recaptcha";
 
 function WorkmateList() {
   const [workmates, setWorkmates] = useState([]);
 
-  useEffect(() => {
+  const loadWorkmates = (e) =>{
+    e.preventDefault();
+
     const workmateService = new WorkmateService();
     const usersPromise = workmateService.getUsers();
 
     usersPromise.then((data) => {
       setWorkmates(data.data)
     }).catch(err => { console.log(err)});
-  }, []);
+  }
 
   return (
     <div>
       <h1>Workmates!</h1>
+
+      <form onSubmit={loadWorkmates}>
+        <input type="submit" className="button-primary u-full-width"/>
+      </form>
+
       <table>
         <tbody>
       {workmates.map((person) => (
